637a11a32c634fb3368331b25f6df1e4e62ef641
Thai Tran <itzthaitran@gmail.com>
2024-05-04 18:12:37 -0600

    added questions, rid of colors

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require ("inquirer")
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the purpose of your project?"
+    }
+]
